b38299cdd8e2ee9d9acc14ee0074e7032768b8a5
kawarimidoll <kawarimidoll+git@gmail.com>
2021-03-13 15:56:47 +0900

    feat: add index.js and defaults.js

---
+++ defaults.js
@@ -0,0 +1,45 @@
+const searchOptions = {
+  searchMaxSuggestions: 10,
+  test: null,
+  searchHotkeys: ["s", "/"],
+};
+
+const blogOptions = {
+  directories: [
+    {
+      id: "post",
+      dirname: "_posts",
+      path: "/",
+      title: "",
+      frontmatter: {
+        home: true,
+      },
+    },
+  ],
+  frontmatters: [
+    {
+      id: "tag",
+      keys: ["tags"],
+      path: "/tags/",
+    },
+    {
+      id: "category",
+      keys: ["categories"],
+      path: "/categories/",
+    },
+  ],
+  globalPagination: {
+    sorter: (prev, next) => (prev.regularPath < next.regularPath ? 1 : -1),
+  },
+};
+
+const seoOptions = {};
+
+const tailwindOptions = {};
+
+module.exports = {
+  searchOptions,
+  blogOptions,
+  seoOptions,
+  tailwindOptions,
+};
---
+++ index.js
@@ -0,0 +1,58 @@
+const defaults = require("./defaults");
+
+module.exports = (option, ctx) => {
+  const { themeConfig } = ctx;
+
+  const blogOptions = Object.assign(
+    defaults.blogOptions,
+    themeConfig.blogOptions || {}
+  );
+
+  const searchOptions = Object.assign(
+    defaults.searchOptions,
+    themeConfig.searchOptions || {}
+  );
+
+  const seoOptions = Object.assign(
+    defaults.seoOptions,
+    themeConfig.seoOptions || {}
+  );
+
+  const tailwindOptions = Object.assign(
+    defaults.tailwindOptions,
+    themeConfig.tailwindOptions || {}
+  );
+
+  return {
+    name: "vuepress-theme-blog-kawarimidoll",
+    plugins: [
+      ["@vuepress/blog", blogOptions],
+      "@vuepress/nprogress",
+      "table-of-contents",
+      "smooth-scroll",
+      [
+        "@vuepress/medium-zoom",
+        {
+          selector: "#content article section :not(a) > img",
+        },
+      ],
+      ["@vuepress/search", searchOptions],
+      ["seo", seoOptions],
+      ["container", { type: "tip" }],
+      ["container", { type: "warning" }],
+      ["container", { type: "danger" }],
+      [
+        "container",
+        {
+          type: "details",
+          before: (info) =>
+            `<details class="custom-block details">${
+              info ? `<summary>${info}</summary>` : ""
+            }\n`,
+          after: () => "</details>\n",
+        },
+      ],
+      ["@kawarimidoll/tailwind", tailwindOptions],
+    ],
+  };
+};
