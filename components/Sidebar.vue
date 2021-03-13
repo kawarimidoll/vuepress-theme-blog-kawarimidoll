7900768041f3a843ca20e044dda58ed861c14669
kawarimidoll <kawarimidoll+git@gmail.com>
2021-03-13 19:24:44 +0900

    feat: add profile to sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,3 +1,43 @@
 <template>
-  <div>this is sidebar!</div>
+  <div id="component-sidebar" class="h-full">
+    <SearchBox />
+
+    <section v-if="profile.avatarUrl || profile.name">
+      <h3 class="mt-0 mb-5">Profile</h3>
+      <div class="flex">
+        <img
+          v-if="profile.avatarUrl"
+          class="block rounded-full"
+          :src="profile.avatarUrl"
+          alt="avatar"
+        />
+
+        <div v-if="profile.name">
+          <p class="my-0">
+            <strong class="name">{{ profile.name }}</strong>
+          </p>
+          <p class="my-0 text-sm">
+            {{ profile.subTitle }}
+          </p>
+        </div>
+      </div>
+
+      <p v-html="profile.descriptionHtml" class="mt-4 mb-0" />
+    </section>
+
+    <TOC class="sticky top-0" />
+  </div>
 </template>
+<script>
+import SearchBox from "@SearchBox";
+export default {
+  components: {
+    SearchBox,
+  },
+  computed: {
+    profile() {
+      return this.$themeConfig.profile || {};
+    },
+  },
+};
+</script>
