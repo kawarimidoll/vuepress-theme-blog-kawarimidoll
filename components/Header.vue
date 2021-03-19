000c6d855680fd2480ab04d259e1abb1eafb3910
kawarimidoll <kawarimidoll+git@gmail.com>
2021-03-19 12:14:04 +0900

    feat: improve header

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,27 @@
+<!-- [初めてでもわかるTailwindcss入門(2)ナビゲーションバー | アールエフェクト](https://reffect.co.jp/html/tailwind-for-beginners-navigation-menu) -->
 <template>
-  <div id="header" class="bg-gray-500 flex align-center justify-center">
-    <header class="container flex justify-between items-center text-white">
-      <RouterLink to="/" class="no-underline text-4xl font-semibold">
-        <component :is="titleTag">
-          {{ $siteTitle }}
-        </component>
-      </RouterLink>
-      <div class="xl:hidden">
-        <button @click="isOpen = !isOpen" class="focus:outline-none">
-          <faIcon :icon="isOpen ? 'times' : 'bars'" class="fa-2x" />
-        </button>
+  <div id="header" class="bg-gray-500 centering-child text-white">
+    <header class="container xl:flex justify-between items-center h-32 xl:h-24">
+      <div class="centering-child mt-6 xl:mt-0">
+        <RouterLink to="/" class="block no-underline text-4xl font-semibold">
+          <component :is="titleTag">
+            {{ $siteTitle }}
+          </component>
+        </RouterLink>
+      </div>
+      <div v-if="navLinks.length > 0" class="centering-child mt-2 h-8">
+        <div class="flex">
+          <RouterLink
+            v-for="item in navLinks"
+            :key="item.path"
+            :to="item.path"
+            :title="item.label"
+            :aria-label="item.label"
+            class="block mx-4"
+          >
+            {{ item.label }}
+          </RouterLink>
+        </div>
       </div>
     </header>
   </div>
@@ -17,14 +29,12 @@
 
 <script>
 export default {
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
   computed: {
     titleTag() {
       return this.$route.path === "/" ? "h1" : "span";
+    },
+    navLinks() {
+      return this.$themeConfig.navLinks || [];
     },
   },
 };
